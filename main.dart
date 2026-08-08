a17eb3983f6882a3f9df4ee7363cef996edc8e21
master <admin@gmail.com>
2026-08-08 09:14:22 +0300

    First commit with payment method

--- main.dart
+++ main.dart
@@ -1,7 +1,17 @@
 void main (){
+paymentmethod paymethod = jaibank();
+paymethod.pay(100.0);
 
 }
 
 abstract class paymentmethod {
   double pay(double amount);
+}
+
+class jaibank implements paymentmethod {
+  @override
+  double pay(double amount) {
+    print("Paying $amount using Jaibank");
+    return amount;
+  }
 }
