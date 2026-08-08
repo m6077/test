659a71c85d92b7cfefbc3afe6b8e81e3223aa9ba
master <admin@gmail.com>
2026-08-08 09:27:39 +0300

    pay jawally

--- main.dart
+++ main.dart
@@ -2,6 +2,9 @@ void main (){
 paymentmethod paymethod = jaibank();
 paymethod.pay(100.0);
 
+paymentmethod paymethod2 = jawallybank();
+paymethod2.pay(200.0);  
+
 }
 
 abstract class paymentmethod {
@@ -12,6 +15,14 @@ class jaibank implements paymentmethod {
   @override
   double pay(double amount) {
     print("Paying $amount using Jaibank");
+    return amount;
+  }
+}
+
+class jawallybank implements paymentmethod {
+  @override
+  double pay(double amount) {
+    print("Paying $amount using Jawallybank ");
     return amount;
   }
 }
